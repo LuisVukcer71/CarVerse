7884fbec7c78ccdbb6454e95bded4165983ad4bd
Luis Meinhardt <luis.meinhardt.wien@gmail.com>
2025-11-12 17:29:44 +0100

    Modell

--- js/model.js
+++ js/model.js
@@ -33,7 +33,7 @@ export function loadModel(scene, collisionObjects) {
     const loader = new GLTFLoader(loadingManager);
 
     loader.load(
-        "assets/MainV1_6.glb",
+        "assets/MainV1_7.glb",
         (gltf) => {
             const model = gltf.scene;
             scene.add(model);
